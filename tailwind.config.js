7f455f0942e4dab33f8808a218b7e2ed7ef6bc6c
dayronfurcain <113483779+dayronfurcain@users.noreply.github.com>
2024-02-27 10:02:32 -0500

    Add new files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'red-hat-display': ['Red Hat Display', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
